2f76eca31ba9092c7f90dd1d410a21cda37313bc
TacioAntonio <tacioantonio10@gmail.com>
2024-05-25 15:00:08 -0300

    feat: initial application modifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HomeComponent } from './components/home/home.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
+import { MyMovieListComponent } from './components/my-movie-list/my-movie-list.component';
+import { AuthenticateGuard } from './shared/guards/authenticate.guard';
+import { Error404Component } from './core/components/error404/error404.component';
+
+export const routes: Routes = [
+  {
+    path: 'my-movie-list/:id',
+    component: MyMovieListComponent,
+    canActivate: [AuthenticateGuard]
+  },
+  {
+    path: 'home/:id',
+    component: HomeComponent,
+    canActivate: [AuthenticateGuard]
+  },
+  {
+    path: 'profiles',
+    component: ProfilesComponent,
+    canActivate: [AuthenticateGuard]
+  },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: '**', component: Error404Component }
+];
